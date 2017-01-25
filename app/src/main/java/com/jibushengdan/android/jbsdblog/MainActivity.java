ad0dd995c323b98a665a1dd3131fae123472226a
huhuhuhu <h573980998@gmail.com>
2017-01-25 14:17:39 +0800

    Initial commit

---
+++ app/src/main/java/com/jibushengdan/android/jbsdblog/MainActivity.java
@@ -0,0 +1,48 @@
+package com.jibushengdan.android.jbsdblog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.CookieManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView=(WebView)findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://jibushengdan.duoshuo.com/login/weibo/");
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // TODO Auto-generated method stub
+                view.loadUrl(url);// 使用当前WebView处理跳转
+                setTitle(url);
+                return true;//true表示此事件在此处被处理，不需要再广播
+            }
+            @Override   //转向错误时的处理
+            public void onReceivedError(WebView view, int errorCode,
+                                        String description, String failingUrl) {
+                // TODO Auto-generated method stub
+                Toast.makeText(MainActivity.this, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                CookieManager cookieManager = CookieManager.getInstance();
+                String CookieStr = cookieManager.getCookie(url);
+                Log.d("sunzn", "Cookies = " + CookieStr);
+                Toast.makeText(MainActivity.this, "Cookies = " + CookieStr, Toast.LENGTH_SHORT).show();
+                if(url.startsWith("http://duoshuo.com/profile/")&&CookieStr.indexOf("duoshuo_unique=")>-1){
+                    Toast.makeText(MainActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
+                }
+                super.onPageFinished(view, url);
+            }
+        });
+    }
+}
